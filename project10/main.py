d168e0fa3213abb1f9c811674ba8b3ff815b455d
Jack Shi <xxruixin@gmail.com>
2025-03-15 22:31:21 -0700

    update project10

--- project10/main.py
+++ project10/main.py
@@ -18,7 +18,7 @@ def main():
         "running": True,
 
         # Display and base image
-        "display_index": 0,
+        "display_index": 1,
         "selected_image": "image1.png",
 
         # Base image transforms
